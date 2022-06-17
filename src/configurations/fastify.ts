031c432f15f231e3075e6eb7fe3d717f36bb1a05
Jintao Koong <jintaokoong99@gmail.com>
2022-06-18 00:54:38 +0800

    feat: add swagger and mongoose

--- src/configurations/fastify.ts
+++ src/configurations/fastify.ts
@@ -1,11 +1,26 @@
 import fastify from "fastify";
+import swagger from "@fastify/swagger";
 import { request } from "http";
-import { GetSongsQueryString } from "schemas/get-songs";
+import {
+  GetSongsQueryString,
+  GetSongsQueryStringSchema,
+} from "schemas/get-songs";
 
 const app = fastify({
   logger: {
     level: "debug",
   },
+});
+app.register(swagger, {
+  routePrefix: "docs",
+  openapi: {
+    info: {
+      title: "Song List API",
+      version: "1.0.0",
+      description: "Song List API Backend",
+    },
+  },
+  exposeRoute: true,
 });
 
 app.get("/", (_, resp) => {
@@ -16,6 +31,10 @@ app.get<{ Querystring: GetSongsQueryString }>("/songs", (req, resp) => {
   const { query } = req;
   app.log.debug({ query });
   resp.send(query);
+});
+
+app.ready(() => {
+  app.swagger();
 });
 
 export default app;
