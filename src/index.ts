9681a0de2212bad66cc025e111b3a7d343a71eab
Jintao Koong <jintaokoong99@gmail.com>
2022-06-18 15:41:12 +0800

    feat: connect mongodb
    - add example env file
    - add code style config

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,28 @@
-import "configurations/firebase";
-import app from "configurations/fastify";
 import { getOr } from "configurations/envvar";
+import app from "configurations/fastify";
+import "configurations/firebase";
+import mongoose from "mongoose";
+
+const connectDatabase = () => mongoose.connect(getOr('MONGO_URL', ''), {
+  dbName: getOr('MONGO_DATABASE', 'sldb'),
+  user: getOr('MONGO_USER', ''),
+  pass: getOr('MONGO_PASS', ''),
+})
+
+const startFastify = () => app
+.listen({
+  port: getOr("PORT", 41867),
+});
 
 const main = () => {
-  return app
-    .listen({
-      port: getOr("PORT", 41867),
-    })
-    .then((value) => {
+  connectDatabase().then(() => {
+    app.log.debug('connected to mongodb');
+    return startFastify().then((value) => {
       app.log.debug(value);
     })
-    .catch((error) => {
-      app.log.error(error);
-    });
+  }).catch(error => {
+    app.log.error(error);
+  })
 };
 
 main();
